4536a2d19fbf817d32beadeebac7be38d02bccac
Louis <32419791+louisLouL@users.noreply.github.com>
2018-04-16 21:11:27 -0400

    Update CacheDecorator.py

--- CacheDecorator.py
+++ CacheDecorator.py
@@ -1,22 +1,22 @@
 class Cache:
 
-    def __init__(self, redis_instance, cache_ttl=3600):
+    def __init__(self, redis_instance):
         self.redis = redis_instance
-        self.cache_ttl = cache_ttl
 
-    def enable(self, func):
-        def func_wrapper(*args, **kwargs):
-            target_key = ":".join(["CACHE", func.__name__, *args, str(kwargs)])
-            target_key = hash(target_key)
-            a = self.redis.get(target_key)
-            if a:
-                return a
-            else:
-                result = func(*args, **kwargs)
-                self.redis.set(target_key, result, self.cache_ttl)
-                return result
-
-        return func_wrapper
+    def ttl(self, ttl=3600):
+        def enable(func):
+            def func_wrapper(*args, **kwargs):
+                target_key = ":".join(["CACHE", func.__name__, *args, str(kwargs)])
+                target_key = hash(target_key)
+                a = self.redis.get(target_key)
+                if a:
+                    return a
+                else:
+                    result = func(*args, **kwargs)
+                    self.redis.set(target_key, result, ttl)
+                    return result
+            return func_wrapper
+        return enable
 
 
 if __name__ == '__main__':
@@ -28,11 +28,13 @@ if __name__ == '__main__':
     cache = Cache(redis)
 
 
-    @cache.enable
-    def time():
+    @cache.ttl(300)
+    def pseudo_calc():
+        sleep(1)
+        print("Compute in progress")
         return str(datetime.now())
 
 
     while True:
-        print(time())
-        sleep(1)
+        print(pseudo_calc())
+        sleep(.1)
